849548f27ece7cc724d3441c0138279317f8b922
Talysson Lima <thalyssonlv@gmail.com>
2023-05-11 09:13:04 -0300

    port 8000 api gateway default

--- config/setebit-package.php
+++ config/setebit-package.php
@@ -7,5 +7,5 @@ return [
         'x-token',
         'x-permissions'
     ],
-    'url_api_gateway' => env('URL_API_GATEWAY', 'https://api.teste.bet'),
+    'url_api_gateway' => env('URL_API_GATEWAY', 'https://api.teste.bet:8000'),
 ];
