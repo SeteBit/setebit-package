458b9c5283c26ee642b4fecb24cbbefc03b2c4e7
Talysson Lima <thalyssonlv@gmail.com>
2023-05-12 09:32:30 -0300

    alias middleware CheckIfTenantHasModule

--- src/SetebitPackageServiceProvider.php
+++ src/SetebitPackageServiceProvider.php
@@ -4,12 +4,15 @@ namespace Setebit\Package;
 
 use Illuminate\Support\ServiceProvider;
 use Setebit\Package\Http\Middleware\BindTheHeader;
+use Setebit\Package\Http\Middleware\CheckIfTenantHasModule;
 
 class SetebitPackageServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        app('router')->aliasMiddleware('auth-data', BindTheHeader::class);
+        app('router')
+            ->aliasMiddleware('auth-data', BindTheHeader::class)
+            ->aliasMiddleware('module', CheckIfTenantHasModule::class);
 
         $this->publishes([
             __DIR__ . '/../config/setebit-package.php' => config_path('setebit-package.php'),
