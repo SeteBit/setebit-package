734f202e58090a6d99fdf8fc3db47771bba7c1b2
Talysson Lima <thalyssonlv@gmail.com>
2023-10-30 11:12:43 -0300

    chore - facade CreateTicketInvoice.php

---
+++ src/Facades/CreateTicketInvoice.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Setebit\Package\Facades;
+
+use GuzzleHttp\Promise\PromiseInterface;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static PromiseInterface|Response handle(int $userId, array $data)
+ */
+class CreateTicketInvoice extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return \Setebit\Package\CreateTicketInvoice::class;
+    }
+}
