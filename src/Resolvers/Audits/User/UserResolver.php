62d75b382ff99d0d0091e0cf7f2a97f54d8377d5
danieldsp0 <dnsouza.dev@gmail.com>
2024-01-02 15:29:40 -0300

    chore: updating UserResolver to be compatible with \OwenIt\Auditing\Contracts\UserResolver

--- src/Resolvers/Audits/User/UserResolver.php
+++ src/Resolvers/Audits/User/UserResolver.php
@@ -6,7 +6,7 @@ use Setebit\Package\Resolvers\Audits\User\UserAuthenticable;
 
 class UserResolver implements \OwenIt\Auditing\Contracts\UserResolver
 {
-    public static function resolve(\OwenIt\Auditing\Contracts\Auditable $auditable): UserAuthenticable
+    public static function resolve(): UserAuthenticable
     {
         return new UserAuthenticable();
     }
