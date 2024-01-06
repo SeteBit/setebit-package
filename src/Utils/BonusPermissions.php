1b4568261390b44b539db0a80f57d2dff2ae0d9c
Talysson Lima <thalyssonlv@gmail.com>
2024-01-06 10:58:54 -0300

    chore: improving bonuses permissions

--- src/Utils/BonusPermissions.php
+++ src/Utils/BonusPermissions.php
@@ -13,49 +13,20 @@ class BonusPermissions
 
     public static string $bind = 'bônus';
 
-    /**
-     * List
-     */
-
-    const LIST_BIND = [
+    const LIST_BONUS = [
         'name' => 'bônus.listar',
         'roles' => [self::ADMIN]
     ];
-    const LIST_BONUS = [
-        'name' => 'bônus.listar.bônus',
-        'roles' => [self::ADMIN]
-    ];
 
-    /**
-     * Store
-     */
-
-    const STORE_BIND = [
+    const STORE_BONUS = [
         'name' => 'bônus.criar',
         'roles' => [self::ADMIN]
     ];
-    const STORE_BONUS = [
-        'name' => 'bônus.criar.bônus',
-        'roles' => [self::ADMIN]
-    ];
 
-    /**
-     * Update
-     */
-
-    const UPDATE_BIND = [
+    const UPDATE_BONUS = [
         'name' => 'bônus.atualizar',
         'roles' => [self::ADMIN]
     ];
-    const UPDATE_BONUS = [
-        'name' => 'bônus.atualizar.bônus',
-        'roles' => [self::ADMIN]
-    ];
-
-
-    /**
-     * Convert
-     */
 
     const CONVERT_BONUS = [
         'name' => 'bônus.converter',
